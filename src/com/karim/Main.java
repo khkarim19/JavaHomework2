e43fae8998fd6ed1ce3923cbfa7b0227f1002382
khkarim19 <khkarim@mail.ru>
2022-02-27 23:52:21 +0300

    Commit 2

--- src/com/karim/Main.java
+++ src/com/karim/Main.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 class Sorter implements Runnable {
     int[] nums;
-    Sorter(int[] n){
+
+    Sorter(int[] n) {
         nums = n;
     }
-    public void run() {
+
+    synchronized public void run() {
         boolean isSorted = false;
         int buf;
         while (!isSorted) {
@@ -30,16 +32,15 @@ public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
         final long nanoSecondsPerMillisecond = 1000000;
-        // int size = rnd.nextInt(10);
         int size = 40000;
         int nums1[] = new int[size];
         for (int i = 0; i < nums1.length; i++) {
             nums1[i] = rnd.nextInt(10);
         }
-        for(int i = 0;i<nums1.length;i++){
-            System.out.print(nums1[i]+" ");
+        System.out.println("Изначальный массив");
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
         }
-        System.out.println("Массив был создан");
         int nums2_1[] = new int[size / 2];
         int nums2_2[] = new int[size / 2];
         System.arraycopy(nums1, 0, nums2_1, 0, 19999);
@@ -54,7 +55,6 @@ public class Main {
             for (int i = 0; i < nums1.length - 1; i++) {
                 if (nums1[i] > nums1[i + 1]) {
                     isSorted = false;
-
                     buf = nums1[i];
                     nums1[i] = nums1[i + 1];
                     nums1[i + 1] = buf;
@@ -63,43 +63,45 @@ public class Main {
         }
         long stopTime1 = System.nanoTime();
         double elapsedTime1 = (stopTime1 - startTime1) / (nanoSecondsPerMillisecond);
-        for(int i = 0;i<nums1.length;i++){
-            System.out.print(nums1[i]+" ");
+        System.out.println("Массив, отсортированный в 1 потоке");
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
         }
         System.out.println();
         System.out.println("Время, потраченное на сортировку пузырьком массива из 40000 элементов (в миллисекундах): " + elapsedTime1);
 
         ////////////////////////////////// С использованием потоков
-
+        long startTime2 = System.nanoTime();
         Thread thread1 = new Thread(new Sorter(nums2_1));
         Thread thread2 = new Thread(new Sorter(nums2_2));
         thread1.start();
         try {
             thread1.join();
+        } catch (InterruptedException ex) {
         }
-        catch(InterruptedException ex) { }
         thread2.start();
-        try{
+        try {
             thread2.join();
+        } catch (InterruptedException ex) {
         }
-        catch(InterruptedException ex) { }
-        long startTime2 = System.nanoTime();
-            int middle = nums2_1.length;
-            int[] nums2 = new int[nums2_1.length + nums2_2.length];
-            for(int i = 0; i < (nums2_1.length + nums2_2.length); i++){
-                if(i < middle){
-                    nums2[i] = nums2_1[i];
-                }
-                else{
-                    nums2[i] = nums2_2[i - middle];
-                }
+        int middle = nums2_1.length;
+        int[] nums2 = new int[nums2_1.length + nums2_2.length];
+        for (int i = 0; i < (nums2_1.length + nums2_2.length); i++) {
+            if (i < middle) {
+                nums2[i] = nums2_1[i];
+            } else {
+                nums2[i] = nums2_2[i - middle];
             }
+        }
         long stopTime2 = System.nanoTime();
         double elapsedTime2 = stopTime2 - startTime2;
-        System.out.println("Время, потраченное на сортировку пузырьком массива из 40000 элементов (в наносекундах): " + elapsedTime2);
-        for(int i = 0;i<nums2.length;i++){
-            System.out.print(nums2[i]+" ");
+        System.out.println("Массив, отсортированный в 2 потока");
+        for (int i = 0; i < nums2.length; i++) {
+            System.out.print(nums2[i] + " ");
         }
+        System.out.println();
+        System.out.println("Время, потраченное на сортировку пузырьком массива из 40000 элементов (в миллисекундах): " + elapsedTime2 / nanoSecondsPerMillisecond);
+
 
     }
 }
